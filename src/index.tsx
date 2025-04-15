d56f454b6805f893400a9aa03100c39b300df13b
Anwar Shah <a.shah@thesynapses.com>
2025-04-15 15:52:48 +0530

    Added Query Client

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
-import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
 import MainStackNavigation from './navigation/Stack/MainStackNavigation';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 const App = () => {
-  return <MainStackNavigation />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <MainStackNavigation />;
+    </QueryClientProvider>
+  );
 };
 
 export default App;
